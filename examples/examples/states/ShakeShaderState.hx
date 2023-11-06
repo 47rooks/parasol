2ef8608b75d632762a7ac2511d7a2f99b19ad3a8
47rooks <47rooks@gmail.com>
2023-11-05 17:26:03 -0800

    change to a button to start shake from checkbox

--- examples/examples/states/ShakeShaderState.hx
+++ examples/examples/states/ShakeShaderState.hx
@@ -2,8 +2,11 @@ package examples.states;
 
 import examples.states.ImagesState;
 import flixel.addons.ui.FlxSlider;
+import flixel.addons.ui.FlxUIButton;
 import flixel.addons.ui.FlxUICheckBox;
 import flixel.system.FlxAssets.FlxShader;
+import flixel.system.FlxAssets;
+import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import parasol.shaders.ShakeShader;
 
@@ -29,21 +32,23 @@ class ShakeShaderState extends ImagesState {
         _shader = new ShakeShader();
 
         // Create controls
-        var enableC = new FlxUICheckBox(Controls.LINE_X, 50, null, null, "Enable Effect", 100, null, toggleShader);
-        enableC.getLabel().size = 15;
+        var button = new FlxUIButton(Controls.LINE_X, 50, "Start shake", buttonCbk);
+		button.resize(200, 40);
+		button.setLabelFormat(14, FlxColor.BLACK, FlxTextAlign.CENTER);
+		add(button);
 
         _blur = new FlxUICheckBox(Controls.LINE_X, 100, null, null, "Use blur", 100);
         _blur.getLabel().size = 15;
 
-        var intensitySlider = new FlxSlider(this, "_intensity", Controls.LINE_X, 170.0, 0.00, 1.00, 300, 15, 3, FlxColor.WHITE, FlxColor.WHITE);
-		intensitySlider.setTexts("Intensity", true, "0.00", "1.00", 15);
+        var intensitySlider = new FlxSlider(this, "_intensity", Controls.LINE_X, 170.0, 0.0, 1.0, 300, 15, 3, FlxColor.WHITE, FlxColor.WHITE);
+		intensitySlider.setTexts("Intensity", true, "0.0", "1.0", 15);
 
         var durationSlider = new FlxSlider(this, "_duration", Controls.LINE_X, 230.0, 0.0, 5.0, 300, 15, 3, FlxColor.WHITE, FlxColor.WHITE);
 		durationSlider.setTexts("Duration (seconds)", true, "0.0", "5.0", 15);
 
         _controls = new Controls(20, 100, 400, 400, [
-            // Add a checkbox to turn the shader on and off
-            enableC,
+            // Add button to start shake
+            button,
             // Add blur while shaking
             _blur,
             // Intensity of shaking
@@ -60,13 +65,18 @@ class ShakeShaderState extends ImagesState {
     /**
      * Toggle the shader on and off, callback for enable checkbox.
      */
-     function toggleShader():Void {
-        if (_sprite.shader == null) {
-            _sprite.shader = _shader;
-            cast(_sprite.shader, ShakeShader).shake(_intensity, _duration, _blur.checked);
-        } else {
-            _sprite.shader = null;
-        }
+    //  function toggleShader():Void {
+    //     if (_sprite.shader == null) {
+    //         _sprite.shader = _shader;
+    //         cast(_sprite.shader, ShakeShader).shake(_intensity, _duration, _blur.checked);
+    //     } else {
+    //         _sprite.shader = null;
+    //     }
+    // }
+
+    function buttonCbk():Void {
+        _sprite.shader = _shader;
+        cast(_sprite.shader, ShakeShader).shake(_intensity, _duration, _blur.checked);
     }
 
     override public function update(elapsed:Float):Void {
